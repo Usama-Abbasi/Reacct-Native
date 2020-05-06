e01ff031d0977b85a8a9b18f1af4400057baf41c
usama <usama999abbasi@gmail.com>
2020-05-06 20:06:07 +0500

    Assignment no 1

--- confusion/component/MainComponents.js
+++ confusion/component/MainComponents.js
@@ -8,12 +8,44 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from './HomeComponent';
 import { Header } from 'react-native/Libraries/NewAppScreen';
-
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 const Stack=createStackNavigator();
 function HomeStack(){
   return(
     <Stack.Navigator>
        <Stack.Screen name="Home" component={Home} options={{
+           headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: "#fff"            
+        }
+        }}/>
+    </Stack.Navigator>
+  );
+}
+function AboutStack(){
+  return(
+    <Stack.Navigator>
+       <Stack.Screen name="About" component={About} options={{
+           headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: "#fff"            
+        }
+        }}/>
+    </Stack.Navigator>
+  );
+}
+function ContactStack(){
+  return(
+       
+    <Stack.Navigator>
+       <Stack.Screen name="Contact" component={Contact} options={{
            headerStyle: {
             backgroundColor: "#512DA8"
         },
@@ -58,7 +90,9 @@ function MyDrawer(){
        drawerStyle={{backgroundColor:'#D1C4E9'}} 
        >
          <Drawer.Screen name="HomeStack" component={HomeStack} options={{title:'Home'}} />
+         <Drawer.Screen name="AboutStack" component={AboutStack} options={{title:'About Us'}} />
          <Drawer.Screen name="MyStack" component={MyStack} options={{title:'Menu'}} />
+         <Drawer.Screen name="ContactStack" component={ContactStack} options={{title:'Contact Us'}} />
          
        </Drawer.Navigator>
   );
