9483b913335cdcf84de7a036a5455ece3e03c835
usama <usama999abbasi@gmail.com>
2020-05-12 14:36:02 +0500

    Persist Redux Store

--- confusion/App.js
+++ confusion/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Main from './component/MainComponents';
 import {ConfigureStore} from './redux/configureStore';
 import {Provider} from 'react-redux';
-const store=ConfigureStore();
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { Loading } from './component/LoadingComponent';
+
+const { persistor, store } = ConfigureStore();
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
-        </Provider>
+        <PersistGate 
+          loading={<Loading />}
+          persistor={persistor}>
+          <Main />
+        </PersistGate>
+      </Provider>
     
     );
   }
--- confusion/redux/configureStore.js
+++ confusion/redux/configureStore.js
@@ -6,9 +6,16 @@ import { comments } from './comments';
 import { promotions } from './promotions';
 import { leaders } from './leaders';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
 export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
+    const config = {
+        key: 'root',
+        storage,
+        debug: true
+      }
+      const store = createStore(
+        persistCombineReducers(config, {
             dishes,
             comments,
             promotions,
@@ -18,5 +25,7 @@ export const ConfigureStore = () => {
         applyMiddleware(thunk, logger)
     );
 
-    return store;
+    const persistor = persistStore(store)
+
+    return { persistor, store };
 }
